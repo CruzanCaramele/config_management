6e595cae74594e98326b471fd04ca7b890dd8d2a
CruzanCaramele <letters2hamza@gmail.com>
2017-12-23 18:23:29 +0800

    add require dependencies

--- examples/package_file_service.pp
+++ examples/package_file_service.pp
@@ -4,11 +4,13 @@ package {'mysql-server':
 }
 
 file {'/etc/mysql/mysql.cnf':
-	source => '/examples/files/mysql.cnf'
-	notify => Service['mysql'],
+	source  => '/examples/files/mysql.cnf',
+	notify  => Service['mysql'],
+	require => Package['mysql-server'],
 }
 
 service {'mysql':
-	ensure => running,
-	enable => true,
+	ensure  => running,
+	enable  => true,
+	require => [Package['mysql-server'], File['/etc/mysql/mysql.cnf']],
 }

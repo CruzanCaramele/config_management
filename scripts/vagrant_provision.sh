39b8b7bbd04c8f6400674f3da6f798ff7cd047f6
CruzanCaramele <letters2hamza@gmail.com>
2018-10-13 23:30:14 +0800

    mv nginx module to puppet modules directory

--- scripts/vagrant_provision.sh
+++ scripts/vagrant_provision.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Variables
-REPO_DEB_URL=https://apt.puppetlabs.com/puppet-release-${DISTRIB_CODENAME}.deb
+REPO_DEB_URL="https://apt.puppetlabs.com/puppet5-release-xenial.deb"
 
 if which  puppet > /dev/null 2>&1; then
     echo "Puppet is already installed"
@@ -16,8 +16,8 @@ apt-get --yes install wget > /dev/null
 # Install the PuppetLabs repo
 echo "Configuring PuppetLabs repo..."
 source /etc/lsb-release
-wget REPO_DEB_URL
-dpkg -i puppet-release-${DISTRIB_CODENAME}.deb
+wget $REPO_DEB_URL
+dpkg -i puppet5-release-xenial.deb
 
 # Do the initial apt-get update
 echo "Initial apt-get update..."
@@ -30,6 +30,6 @@ apt-get -y install git puppet-agent
 echo "Puppet installed!"
 
 echo 'Defaults        secure_path="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin:/opt/puppetlabs/bin:/opt/puppetlabs/puppet/bin"' >/etc/sudoers.d/puppet
-/opt/puppetlabs/puppet/bin/gem install gpgme --no-rdoc --no-ri
-/opt/puppetlabs/puppet/bin/gem install hiera-eyaml-gpg --no-rdoc --no-ri
-/opt/puppetlabs/puppet/bin/gem install r10k --no-rdoc --no-ri
+# /opt/puppetlabs/puppet/bin/gem install gpgme --no-rdoc --no-ri
+# /opt/puppetlabs/puppet/bin/gem install hiera-eyaml-gpg --no-rdoc --no-ri
+# /opt/puppetlabs/puppet/bin/gem install r10k --no-rdoc --no-ri

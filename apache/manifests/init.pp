fb3614178846c8a6a8436f53935dc89f1d9557a0
CruzanCaramele <letters2hamza@gmail.com>
2019-02-01 23:34:33 +0800

    align variables

--- apache/manifests/init.pp
+++ apache/manifests/init.pp
@@ -3,7 +3,7 @@ class apache (
 	$config_path       = $apache::params::config_path,
 	$config_source     = $apache::params::config_source,
 	$service_name      = $apache::params::service_name,
-  	$vhosts_dir        = $apache::params::vhosts_dir,
+  $vhosts_dir        = $apache::params::vhosts_dir,
 	$vhosts_servername = $apache::params::vhosts_servername,
 	String $service_enable,
 	String $config_ensure,

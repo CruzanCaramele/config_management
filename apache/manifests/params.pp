0baff6702216326506159a51d8f8835729120d6d
CruzanCaramele <letters2hamza@gmail.com>
2019-02-01 11:15:32 +0800

    add servername parameter

--- apache/manifests/params.pp
+++ apache/manifests/params.pp
@@ -1,11 +1,12 @@
 class apache::params.pp {
 	case $::osfamily {
 		'RedHat' : {
-			$install_package = 'httpd'
-      $service_name    = 'httpd'
-			$config_path     = '/etc/httpd/conf/httpd.conf'
-			$config_source   = puppet:///modules/apache/httpd.conf
-			$vhosts_dir      = '/etc/httpd/conf.d'
+			$install_package   = 'httpd'
+      $service_name      = 'httpd'
+			$config_path       = '/etc/httpd/conf/httpd.conf'
+			$config_source     = puppet:///modules/apache/httpd.conf
+			$vhosts_dir        = '/etc/httpd/conf.d'
+			$vhosts_servername = 'www.the-puppet-project.com'
 		}
 	}
 }
